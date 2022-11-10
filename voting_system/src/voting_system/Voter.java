4459ad34d4bc17a17b61068849576b630b2734be
ACCOLITE\marek.tomecek <marek.tomecek@accolitedigital.com>
2022-11-10 18:28:44 +0000

    Add Business Logic
    -create Voter logic
    -create VotingSystem logic

--- voting_system/src/voting_system/Voter.java
+++ voting_system/src/voting_system/Voter.java
@@ -1,4 +1,19 @@
 package voting_system;
 
-public class Voter {
+import java.util.Random;
+
+import static voting_system.VotingSystem.candidateVotes;
+
+public class Voter implements Runnable {
+
+    @Override
+    public void run() {
+        Random random = new Random();
+        int vote = (int) random.nextInt(3);
+        System.out.println(Thread.currentThread().getName()+" casting vote for candidate "+(vote+1));
+
+        synchronized (candidateVotes){
+            candidateVotes[vote] = candidateVotes[vote]+1;
+        }
+    }
 }
--- voting_system/src/voting_system/VotingSystem.java
+++ voting_system/src/voting_system/VotingSystem.java
@@ -1,4 +1,34 @@
 package voting_system;
 
+
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
 public class VotingSystem {
+
+    static int[] candidateVotes ={0,0,0};
+    public static void main(String[] args) throws InterruptedException {
+
+        //set main thread name for debugging
+        Thread.currentThread().setName("VotingSystem");
+
+        //initialization
+        long start = System.currentTimeMillis();
+        long end = start + 30 * 1000;
+        int voterNumber = 0;
+
+
+        //main business logic
+        while (System.currentTimeMillis() < end) {
+            System.out.println();
+            new Thread(new Voter(), "Voter number "+voterNumber).start();
+            voterNumber++;
+            Thread.sleep(10);
+        }
+
+        //metrics
+        System.out.println("The votes for candidates are: "+ Arrays.toString(candidateVotes));
+
+
+    }
 }
